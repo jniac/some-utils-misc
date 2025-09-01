85f347c0c48012e93e4b759d1a023f153dda1bc8
jniac <joseph.merdrignac@gmail.com>
2025-09-01 13:35:45 +0200

    fps meter

---
+++ src/fps-meter/fps-meter.tsx
@@ -0,0 +1,19 @@
+import { HTMLProps } from 'react'
+
+import { useEffects } from 'some-utils-react/hooks/effects'
+
+import { useThree } from '../three-provider/hooks'
+
+export function FpsMeter(props: HTMLProps<HTMLDivElement>) {
+  const three = useThree()
+  const { ref } = useEffects<HTMLDivElement>(function* () {
+    yield three.ticker.onTick(() => {
+      ref.current.innerText = `-- ${three.averageFps.toFixed(1)} fps`
+    })
+  }, [])
+  return (
+    <div ref={ref} {...props}>
+      -- fps
+    </div>
+  )
+}
