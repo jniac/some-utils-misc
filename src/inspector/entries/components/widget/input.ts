cb2d059b421d706acf27788dec9c3f6f550ff08a
jniac <joseph.merdrignac@gmail.com>
2025-09-28 21:47:53 +0200

    typo (2)

--- src/inspector/entries/components/widget/input.ts
+++ src/inspector/entries/components/widget/input.ts
@@ -3,7 +3,7 @@ import { DestroyableObject } from 'some-utils-ts/types'
 
 import { FieldComponent } from '../base'
 
-import css from './input.css.ts'
+import css from './input.css'
 
 type Pointer = { x: number; y: number; shiftKey: boolean; altKey: boolean; ctrlKey: boolean; metaKey: boolean }
 type EnumLike = { [key: string]: string | number }
